d50f617cadf27a438423d07c39e02655d96fc03b
Allan Clark <allanc@chickenandporn.com>
2015-04-05 16:48:27 -0700

    improved build logic

---
+++ navtreedata.js
@@ -0,0 +1,29 @@
+var NAVTREE =
+[
+  [ "wwndesc", "index.html", [
+    [ "JavaDoc API Markup for wwndesc", "index.html", null ],
+    [ "README", "md_htdocs_README.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Commandline Options", "cmdopt.html", null ],
+    [ "Data Structures", null, [
+      [ "Data Structures", "annotated.html", "annotated" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
